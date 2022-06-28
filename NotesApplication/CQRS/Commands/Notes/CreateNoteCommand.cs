f9fad044f475ac029229b59555240d654ad7e443
Nick Lychagin <flogger19@rambler.ru>
2022-06-28 21:50:25 +0300

    Start CQRS

---
+++ NotesApplication/CQRS/Commands/Notes/CreateNoteCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Notes.Application.CQRS.Commands.Notes
+{
+    public class CreateNoteCommand : IRequest<Guid>
+    {
+        public CreateNoteCommand(Guid userId, string title, string content)
+        {
+            UserId = userId;
+            Title = title;
+            Content = content;  
+        }
+
+        public Guid UserId { get; }
+
+        public string Title { get; }
+
+        public string Content { get; }
+    }
+}
---
+++ NotesApplication/CQRS/Handlers/Notes/CreateNoteCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Notes.Application.CQRS.Commands.Notes;
+using Notes.Application.Interfaces;
+using NotesDomain;
+
+namespace Notes.Application.CQRS.Handlers.Notes
+{
+    public class CreateNoteCommandHandler : IRequestHandler<CreateNoteCommand, Guid>
+    {
+        private readonly INotesDbContext _dbContext;
+
+        public CreateNoteCommandHandler(INotesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Guid> Handle(CreateNoteCommand request, CancellationToken cancellationToken)
+        {
+            var note = new Note
+            {
+                UserId = request.UserId,
+                Title = request.Title,
+                Content = request.Content,
+                NoteId = Guid.NewGuid(),
+                CreationDate = DateTime.Now,
+            };
+
+            await _dbContext.Notes.AddAsync(note, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return note.NoteId;
+        }
+    }
+}
